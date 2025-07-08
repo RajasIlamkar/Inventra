ed1d9ed8dadbe465af19f870b19c0567aa53fbd5
Rajas Ilamkar <113300283+RajasIlamkar@users.noreply.github.com>
2025-07-08 14:37:28 +0530

    Update Signup.js

--- frontend/src/pages/Signup.js
+++ frontend/src/pages/Signup.js
@@ -8,7 +8,7 @@ function Signup({ setIsLoggedIn }) {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const res = await fetch('http://localhost:5000/api/auth/signup', {
+    const res = await fetch(`${process.env.REACT_APP_BACKEND_URL}/api/auth/signup`, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(formData),
